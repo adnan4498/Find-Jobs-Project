7bf5bafc30372f342eb80e2fe734aac6a4d68ae9
adnan4498 <akhan123456008@gmail.com>
2025-06-04 04:03:53 +0500

    Preloaded fonts in index.html via link and added manualChunks in vite.config.js. splitting react and antd in their own chunks

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split common heavy libraries into their own chunks
+          react: ['react', 'react-dom'],
+          antd: ['antd'],
+        },
+      },
+    },
+  },
 })
